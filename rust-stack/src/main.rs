1a4fdd7a2c63c97fe53e4f4ece70b9399c89489e
Dave Bauman <baumandm@gmail.com>
2014-12-15 13:05:44 -0800

    Add REPL loop

--- rust-stack/src/main.rs
+++ rust-stack/src/main.rs
@@ -1,27 +1,44 @@
+use std::io;
+
 use stack::Stack;
 
 mod stack;
 
 fn main() {
     let mut my_stack = Stack::new();
-    println!("Current Stack: {}", my_stack.to_string());
-    println!("Has Next? {}", my_stack.has_next());
-    
-    my_stack = my_stack.push(1);
-    println!("Current Stack: {}", my_stack);
+    loop {
+        print!("> ");
+        let input = io::stdin().read_line().ok().expect("Failed to read line");
+        let trimmed_input = input.trim_right_chars('\n');
 
-    my_stack = my_stack.push(2);
-    my_stack = my_stack.push(3);
-    println!("Current Stack: {}", my_stack);
-    
-    println!("Has Next? {}", my_stack.has_next());
+        let mut split = trimmed_input.split_str(" ");
 
-    while my_stack.has_next() {
+        match split.next().unwrap() {
+            "q" => break,
+            "push" => {
+                match split.next() {
+                    None => println!("! Provide a value to push !"),
+                    Some(i) => {
+                        let num = from_str(i);
+                        match num {
+                            None => println!("! Value must be numeric !"),
+                            Some(i) => my_stack = my_stack.push(i)
+                        }
+                    }
+                };
+            },
+            "pop" => {
+                if my_stack.has_next() {
+                    let (tail, val) = my_stack.pop();
+                    println!("Popped: {}", val);
+                    my_stack = tail;
+                } else {
+                    println!("! Can't pop any more !");
+                }
+            },
+            _ => println!("! What?? (q/pop/push) !")
+        };
 
-        let (tail, val) = my_stack.pop();
-        println!("Popped: {}", val);
-
-        my_stack = tail;
         println!("Current Stack: {}", my_stack);
     }
     
